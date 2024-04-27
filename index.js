04494c0d50a5b2b2909d154dff1cff9786040a79
Sam <sambolton.kw@gmail.com>
2024-04-27 19:27:56 -0400

    inquire prompt

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const inquirer =require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square} = require('./lib/shapes');
+
+//User input prompt
+inquirer
+    .createPromptModule([
+        {
+            type: 'input',
+            message: 'Enter up to three characters',
+            name: 'text'
+        },
+        {
+            type: 'input',
+            message: 'Enter color or hexadecimal number for text color',
+            name: 'textColor'
+        },
+        {
+            type: 'list',
+            message: 'Choose a shape.',
+            choices: ['Circle', 'Triangle', 'Square'],
+            name: 'shape'
+        },
+        {
+            type: 'input',
+            message: 'Enter a color or hexadecimal number for the shape',
+            name: 'shapeColor'
+        },
+    ])
