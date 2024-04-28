cdb182895989831037da2498dd88db4668037661
Sam <sambolton.kw@gmail.com>
2024-04-28 00:36:34 -0400

    writes files, but needs more work

--- index.js
+++ index.js
@@ -30,9 +30,37 @@ inquirer
     //process inquirer input
     .then((data) => {
         console.log(data)
+   //Create SVG 
+   let svg = '';
+   switch (data.shape) {
+    case 'Triangle':
+        const triangle = new Triangle(data.shapeColor);
+        svg = triangle.render();
+        break;
 
+    case 'Circle':
+        const circle = new Circle(data.shapeColor);
+        svg = circle.render();
+        break;
+
+    case 'Square':
+        const square = new Square(data.shapeColor);
+        svg = square.render();
+        break;    
+    
+    default:
+        console.log('err')
+        };
+
+        console.log('SVG', svg);
+
+        //writing svg file
+        fs.writeFileSync('output.html', svg);
+        console.log('File created')
     })
+     
+    .catch((error) =>{
+        console.error('Error occurred', error);
+    });
 
-    //Create SVG 
-
-    //Function to write SVG
+ 
